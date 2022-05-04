778ee5d4b1fa5aeb41f48753ebf2a3bdc0a58a49
TrvMarco <trevisanello.marco@gmail.com>
2022-05-04 15:37:22 +0200

    corretto codice programma

--- snack-2/script.js
+++ snack-2/script.js
@@ -3,6 +3,8 @@ const secondWord = prompt("Inserisci la seconda parola")
 
 if( firstWord.length > secondWord.length){
     console.log(secondWord, firstWord);
-}else if ( firstWord.length > secondWord.length ){
+}else if ( secondWord.length > firstWord.length ){
     console.log(firstWord, secondWord);
+}else{
+    console.log("Entrambe le parole hanno la stessa lunghezza")
 }
