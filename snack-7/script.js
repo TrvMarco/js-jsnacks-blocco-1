d13bee71bd86d2d66bef8dcc2b4212647bcf2de8
TrvMarco <trevisanello.marco@gmail.com>
2022-05-04 17:37:45 +0200

    abbozzato codice

---
+++ snack-7/script.js
@@ -0,0 +1,25 @@
+// Generatore di "nomi cognomi" casuali: il Grande Gatsby ha  una lista di nomi e una lista di cognomi, e da queste vuole generare una falsa lista di invitati con nome e cognome.
+
+// 1. Creo Array con lista di nomi
+const nameList = ["Marco","Luca", "Giovanni", "Paolo", "Nicola"];
+const surnameList = ["Rossi", "Bianchi", "Verdi", "Pinco", "Pallino"];
+
+for ( let i; i < nameList.length; i++ ){
+
+}
+
+
+
+
+
+
+// 1. creo ciclo per stampare i nomi
+// for (let i = 0; i < nameList.length; i++){
+//     console.log(nameList[i]);
+// }
+
+// for (let i = 0; i < surnameList.length; i++){
+//     console.log(surnameList[i]);
+// }
+
+// console.log(nameList+surnameList)
