69153a3d71afd0567505b3864d87f9d35c83f93e
TrvMarco <trevisanello.marco@gmail.com>
2022-05-04 15:55:56 +0200

    fixato codice programma

--- snack-3/script.js
+++ snack-3/script.js
@@ -1,10 +1,8 @@
-// Il software deve chiedere per 10 volte all’utente di inserire un numero. Il programma stampa la somma di tutti i numeri inseriti.
 let userNumbersTotal = 0
-
-for( i = 0; i <= 10; i++ ){
+// Il software deve chiedere per 10 volte all’utente di inserire un numero. Il programma stampa la somma di tutti i numeri inseriti.
+for( i = 0; i < 10; i++ ){
     let userNumbers = Number(prompt("inserisci un numero"));
+    console.log(userNumbers)
+    userNumbersTotal = userNumbersTotal + userNumbers;
 }
-userNumbersTotal =+ userNumbers; 
 console.log(userNumbersTotal)
-
-// const allNumbers = userNumbers
