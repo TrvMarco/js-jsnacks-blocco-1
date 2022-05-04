aac251b48d3f506a527053c2bf34e697d07564b2
TrvMarco <trevisanello.marco@gmail.com>
2022-05-04 16:24:50 +0200

    aggiunto condizione

--- snack-6/script.js
+++ snack-6/script.js
@@ -1,3 +1,8 @@
 // 1 Inserisci un numero con un promt
-const userNumber = Number
+const userNumber = Number(prompt("Inserisci un numero"))
 // 2.se è pari stampa il numero, se è dispari stampa il numero successivo.
+if(userNumber % 2 == 0){
+    console.log(userNumber)
+}else{
+    console.log(userNumber + 1)
+}
