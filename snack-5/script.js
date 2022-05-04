1d4019a693ec663fd61bc47384cbfde448772382
TrvMarco <trevisanello.marco@gmail.com>
2022-05-04 12:58:47 +0200

    fixato parentesi

--- snack-5/script.js
+++ snack-5/script.js
@@ -1,12 +1,12 @@
 // Crea un array vuoto.
-let empity = [];
+const empity = [];
+
 // Chiedi per 6 volte all’utente di inserire un numero, se è dispari inseriscilo nell’array.
 for (i = 0; i < 6; i++){
-    let userNumber = Number(prompt("Inserisci un numero"))
-    console.log( userNumber )
+    const userNumber = Number(prompt("Inserisci un numero"))
     if ( userNumber % 2 == 1 ){
-        empity.push[ userNumber ]
+        empity.push(userNumber)
     }
 }
 
-console.log(empity)
+console.log(empity);
