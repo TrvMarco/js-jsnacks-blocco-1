39c6f182b8ae36c7250f32489f9b18dbfab430a1
TrvMarco <trevisanello.marco@gmail.com>
2022-05-05 09:54:28 +0200

    esercizio corretto

--- snack-8/script.js
+++ snack-8/script.js
@@ -1,5 +1,14 @@
 // Crea un array di numeri interi 
-const numberList = [1,2,3,4,5,6,7,8,9,10]
+const numberList = [1,2,3,4,5,6,7,8,9,10];
 
-console.log(numberList)
-// e fai la somma di tutti gli elementi  che sono in posizione dispari
+
+let total = 0;
+
+
+for (let i = 0; i < numberList.length; i++){
+    if (i % 2 === 1){
+        total += numberList[i];
+    }
+}
+console.log(total)
+
